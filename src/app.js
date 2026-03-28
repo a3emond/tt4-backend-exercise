84a3ae8d6e7dd4a5b323171f7207126a11a4f0e7
a3emond <a3emond@gmail.com>
2026-03-28 10:44:35 -0400

    feat: add route mounting for authentication, tasks, and users

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+// route mounting
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/users", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("<h1>TT4 Backend</h1><br><p>Welcome to the TT4 Backend API!</p>");
 });
